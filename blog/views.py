bbc950a4968d8bd63d644d9e82c28ef9ca7aa3c8
endri <endri.zylali@gmail.com>
2018-05-15 17:56:50 +0200

    Fixing small bugs for comment form

--- blog/views.py
+++ blog/views.py
@@ -12,7 +12,7 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 class AboutView(TemplateView):
-    template_name = 'about.html'
+    template_name = './blog/about.html'
 
 class PostListView(ListView):
     model = Post
@@ -50,21 +50,23 @@ class DraftListView(LoginRequiredMixin,ListView):
 def add_comment_to_post(request,pk):
     post = get_object_or_404(Post,pk=pk)
 
-    if request.method == 'POST':
+    if request.method == "POST":
         form = CommentForm(request.POST)
         if form.is_valid():
             comment = form.save(commit=False)
             comment.post = post
+            comment.save()
             return redirect('post_detail',pk=post.pk)
     else:
         form = CommentForm()
-    return render(request,'blog/comment_form.html',context={'form':form})
+    return render(request,'blog/comment_form.html',{'form':form})
 
 @login_required
 def comment_approve(request,pk):
     comment = get_object_or_404(Comment,pk=pk)
+    _pk = comment.post.pk
     comment.approve()
-    return redirect('post_detail',pk=comment.post.pk)
+    return redirect('post_detail',pk=_pk)
 
 @login_required
 def comment_remove(request,pk):
